1662dead60d729157dd093eb72a7bb09a8ee4103
“camilavmoura” <camilavilelam@gmail.com>
2021-01-04 19:09:46 +0000

    UPDATE: javascript (unfinished)

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,6 @@
 $(".submit").click(function () {
     if ($("#name-answer"&&'#email-answer'&&"#question-answer").val() == 0) {
-        $(".validation").addClass("show-content");
+        alert("Please fill in all fields properly.")
     }
     else (
         alert("Your message has been sent with success!")
